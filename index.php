ec427f112f52dfc8d81b66ffc6faf553caf6feff
IvineCabrelle <144458237+IvineCabrelle@users.noreply.github.com>
2023-09-29 10:56:12 -0400

    init

---
+++ index.php
@@ -0,0 +1,9 @@
+!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>exercise3</title>
+</head>
+    <body>
+        <form method="post" action="formResult.php">
